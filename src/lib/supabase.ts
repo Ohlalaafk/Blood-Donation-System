51ff2733015a687254458cd333db842177f2d062
Ohlalaafk <houtmeng534@gmail.com>
2025-03-19 11:15:25 +0000

    commit

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,8 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
 import type { Database } from "../types/supabase";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || "";
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || "";
+const supabaseUrl = "https://iuybhwwjbaqcdkgwwuuz.supabase.co";
+const supabaseAnonKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml1eWJod3dqYmFxY2RrZ3d3dXV6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDIzODI4MDEsImV4cCI6MjA1Nzk1ODgwMX0.KQCfaG17MDl4S0ipADAAvg_gsH7eILaXy6c3ZycQWB4";
 
 if (!supabaseUrl || !supabaseAnonKey) {
   console.error(
